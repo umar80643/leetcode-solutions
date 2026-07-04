bc1c65f3bac9a81beac9ed88d556146df7c7993c
Umar Farooque <umar80643@gmail.com>
2026-07-04 12:26:41 +0530

    leetcode 2 solution

---
+++ leetcode2.py
@@ -0,0 +1,19 @@
+dummy = ListNode(0)
+tail = dummy
+carry = 0
+while l1 or l2 or carry:
+    x = l1.val if l1 else 0
+    y = l2.val if l2 else 0
+
+    total = x + y + carry
+    carry = total // 10
+
+    tail.next = ListNode(total % 10)
+    tail = tail.next
+
+    if l1:
+        l1 = l1.next
+    if l2:
+        l2 = l2.next
+return dummy.next
+
