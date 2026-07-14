b008d10bdd3f9cc2ad0cc7dff5d6658c85d77b50
umar farooque <umar80643@gmail.com>
2026-07-14 23:23:03 +0530

    Update leetcode228.py

--- leetcode228.py
+++ leetcode228.py
@@ -1,13 +1,17 @@
 nums = [0,1,2,4,5,7]
 l=[]
 start= nums[0]
-if len(nums)<1:
+if not nums:
     print(0)
-for i in range(1,len(nums)):
-    if nums[i]-nums[i-1]!=1:
-        l.append([start,nums[i-1]])
-        start=nums[i]
-l.append([start,nums[-1]])
+else:
+    l=[]
+    start= nums[0]
+    
+    for i in range(1,len(nums)):
+        if nums[i]-nums[i-1]!=1:
+            l.append([start,nums[i-1]])
+            start=nums[i]
+    l.append([start,nums[-1]])
 ans=[]
 for first,second in l:
     if first==second:
