0eafc44d484aa7d55bef2807e9f4125eb1741955
Umar Farooque <umar80643@gmail.com>
2026-07-19 15:34:30 +0530

    leetcode 234 solution

---
+++ leetcode 234.py
@@ -0,0 +1,21 @@
+head = [1,2,2,1]
+
+slow = fast = head
+while fast and fast.next:
+    slow = slow.next
+    fast = fast.next.next
+
+prev = None
+while slow:
+    nxt = slow.next
+    slow.next = prev
+    prev = slow
+    slow = nxt
+left = head
+right = prev
+while right:
+    if left.val != right.val:
+        return False
+    left = left.next
+    right = right.next
+return True
