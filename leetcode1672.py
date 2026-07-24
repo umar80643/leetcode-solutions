91bb39e0e0d06fd3f9a5981e8f8e763b3a65eadc
Umar Farooque <umar80643@gmail.com>
2026-07-24 23:01:26 +0530

    leetcode 1672 update

--- leetcode1672.py
+++ leetcode1672.py
@@ -1,8 +1,5 @@
 accounts = [[1,2,3],[3,2,1]]
-curr_sum = 0
-for i in range(len(accounts)):
-    best=0
-    for j in range(len(accounts[0])):
-        curr_sum += accounts[i][j]
-    best = max(curr_sum, best)
+best=0
+for account in accounts:
+    best = max(best ,sum(account))
 print(best)
