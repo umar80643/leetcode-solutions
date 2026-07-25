8729c318d93006d16464009d165efc64ca35a244
Umar Farooque <umar80643@gmail.com>
2026-07-25 19:10:17 +0530

    leetcode 977 solution

--- leetcode1672.py
+++ leetcode1672.py
@@ -2,4 +2,4 @@ accounts = [[1,2,3],[3,2,1]]
 best=0
 for account in accounts:
     best = max(best ,sum(account))
-print(best)
+print(best)  
---
+++ leetcode977.py
@@ -0,0 +1,4 @@
+nums = [-4,-1,0,3,10]
+l =sorted([x**2 for x in nums])
+
+print(l)
