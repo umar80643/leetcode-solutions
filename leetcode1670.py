7a786083f58012f1d4d0c6e808c7835145003d1f
Umar Farooque <umar80643@gmail.com>
2026-08-09 12:47:42 +0530

    leetcode 1670 solution

---
+++ leetcode1670.py
@@ -0,0 +1,52 @@
+import queue
+
+
+class FrontMiddleBackQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def pushFront(self, val: int) -> None:
+        self.queue.insert(0,val)
+
+
+
+    def pushMiddle(self, val: int) -> None:
+        mid = (len(self.queue)-1)//2
+        self.queue.insert(mid,val)
+
+
+    def pushBack(self, val: int) -> None:
+        self.queue.append(val)
+
+
+    def popFront(self) -> int:
+        if self.queue:
+            return self.queue.pop(0)
+        return -1
+
+    def popMiddle(self) -> int:
+        if self.queue:
+            mid = (len(self.queue)-1)//2
+            return self.queue.pop(mid)
+        return -1
+
+
+    def popBack(self) -> int:
+        if self.queue:
+            return self.queue.pop()
+        return -1
+
+
+
+q = FrontMiddleBackQueue();
+q.pushFront(1);
+q.pushBack(2);
+q.pushMiddle(3);
+q.pushMiddle(4);
+q.popFront();
+q.popMiddle();
+q.popMiddle();
+q.popBack()
+q.popFront();
+
