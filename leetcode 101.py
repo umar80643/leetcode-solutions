ca3da8be0c55db8fee35086b5c224f90ad308e5a
Umar Farooque <umar80643@gmail.com>
2026-09-06 13:31:34 +0530

    leetcode 101 solution

---
+++ leetcode 101.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isMirror(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+
+            return (
+                    isMirror(left.left, right.right) and
+                    isMirror(
+                        left.right,
+                        right.left
+                    )
+            )
+
+        return isMirror(
+            root.left,
+            root.right
+        )
+
