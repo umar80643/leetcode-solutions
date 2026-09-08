0e0dc8b1eea0ca50b3f646fd394de2881328b3f1
Umar Farooque <umar80643@gmail.com>
2026-09-08 15:31:15 +0530

    leetcode 104 solution

---
+++ leetcode 104.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left , right)
